cdeb557fba39a6630c9ea11e0425b79aaccd7d37
Ciska V <ciskavriezenga@gmail.com>
2024-11-12 13:10:58 +0100

    speaker example

---
+++ blok2b/session1/3_speaker/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+class Tweeter{
+public:
+  // constructor
+  Tweeter();
+  Tweeter(float diam);
+
+  float diameter;
+  float conePosition;
+};
+
+Tweeter::Tweeter() {
+  std::cout << "Tweeter - default constructor\n";
+  // NOTE: duplicate code alarm!!
+  // constructor delegation is a better strategy!
+
+  // in cm
+  diameter = 2.5f;
+  // number with an f --> indicate it is a float
+  conePosition = 0.0f;
+}
+
+Tweeter::Tweeter(float diam) {
+  std::cout << "Tweeter - constructor with float diam parameter\n";
+  // in cm
+  diameter = diam;
+  // number with an f --> indicate it is a float
+  conePosition = 0.0f;
+}
+
+class Speaker{
+public:
+  Speaker();
+  Tweeter aTweeter;
+};
+
+Speaker::Speaker() {
+  std::cout << "Speaker - constructor\n";
+  aTweeter.diameter = 2.4;
+  std::cout << "The diameter of the tweeter is: "
+    << aTweeter.diameter << "\n";
+}
+
+// TODO - add woofer
+
+int main() {
+  Speaker aSpeaker;
+};
+

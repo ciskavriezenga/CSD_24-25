14b6f7be84cfa38df5c282cb4ad24e166b8862b5
Dean <daan.k.schrier@gmail.com>
2025-01-30 16:01:45 +0100

    working trem

--- blok2c/theorie/session1/01_tremeloSession/callback.cpp
+++ blok2c/theorie/session1/01_tremeloSession/callback.cpp
@@ -1,19 +1,25 @@
 #include "callback.h"
 
+CustomCallback::CustomCallback (float sampleRate)
+  : AudioCallback (sampleRate), samplerate (sampleRate) {}
 
-void CustomCallback::prepare(int rate) {
-    samplerate = (float) rate;
-    std::cout << "\nsamplerate: " << samplerate << "\n";
-    tremolo.prepare(rate);
+void CustomCallback::prepare (int rate) {
+  samplerate = (float) rate;
+  std::cout << "\nsamplerate: " << samplerate << "\n";
+  tremolo.prepare (samplerate);
 }
 
-void CustomCallback::process(AudioBuffer buffer) {
-  auto [inputChannels, outputChannels, numInputChannels, numOutputChannels, numFrames] = buffer;
+void CustomCallback::process (AudioBuffer buffer) {
+  auto [inputChannels,
+        outputChannels,
+        numInputChannels,
+        numOutputChannels,
+        numFrames] = buffer;
 
-  for (int channel = 0u; channel < numInputChannels; channel++) {
-    for (int i = 0u; i < numFrames; i++) {
+  for (int channel = 0; channel < numInputChannels; channel++) {
+    for (auto i = 0; i < numFrames; i++) {
       buffer.outputChannels[channel][i] =
-              tremolo.processFrame(buffer.inputChannels[channel][i]);
+        tremolo.processFrame (buffer.inputChannels[channel][i]);
     }
   }
 }

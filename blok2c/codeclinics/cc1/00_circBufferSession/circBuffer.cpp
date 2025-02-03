884c7dfba8c517c56e508b77c1d491e14b3d139b
Ciska V <ciskavriezenga@gmail.com>
2025-02-03 10:47:58 +0100

    example created during the session

--- blok2c/codeclinics/cc1/00_circBufferSession/circBuffer.cpp
+++ blok2c/codeclinics/cc1/00_circBufferSession/circBuffer.cpp
@@ -2,11 +2,33 @@
 
 
 CircBuffer::CircBuffer(int size, int numSamplesDelay)
+  : readHead(0), writeHead(numSamplesDelay)
 {
-  // TODO - init buffer
+  // TODO - init dynamic buffer
 }
 
 CircBuffer::~CircBuffer()
 {
   // TODO - release the dynamic allocated array
 }
+
+float CircBuffer::read() {
+  return buffer[readHead];
+}
+
+void CircBuffer::write(float input) {
+  buffer[writeHead] = input;
+}
+
+void CircBuffer::tick() {
+  incrementHead(readHead);
+  incrementHead(writeHead);
+}
+
+void CircBuffer::incrementHead(int& head) {
+  head++;
+  if(head >= SIZE_ARRAY) {
+    head -= SIZE_ARRAY;
+  }
+}
+
